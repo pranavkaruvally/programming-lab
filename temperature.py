8679b855ce4a8842f760e4a5ac9b0b544ed5e0d4
Your Name <pranavsdreams@gmail.com>
2022-10-24 23:45:43 +0530

    Finished temperature program

---
+++ temperature.py
@@ -0,0 +1,5 @@
+#This program converts degree celcius to degree fahrenheit
+
+celc = float(input("Enter temperature in degree celcius: "))
+fahr = 9*celc/5 + 32
+print("Temperature in degree fahrenheit:", fahr)
