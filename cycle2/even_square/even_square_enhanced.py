a1153207d8ae22954c2b568dddac762019e55821
pranav <pranavsdreams@gmail.com>
2022-12-12 10:02:52 +0530

    Changed floored_sqrt to ceiled_sqrt

--- cycle2/even_square/even_square_enhanced.py
+++ cycle2/even_square/even_square_enhanced.py
@@ -1,30 +1,32 @@
 def is_everydigit_even(n):
-	while n>0:
-		if (n%10)%2 != 0:
-			return False
-		n //= 10
-	return True
+    while n>0:
+        if (n%10)%2 != 0:
+            return False
+        n //= 10
+    return True
 
-def floored_sqrt(n):
-	if n==0 or n==1:
-		return n
-	start, stop = 1, n//2
-	while start < stop: 
-		mid = (start + stop) // 2
-		if mid*mid == n:
-			return mid
-		elif mid*mid < n:
-			if start == mid:
-				return mid
-			start = mid
-		else:
-			if stop == mid:
-				return mid
-			stop = mid
-	return mid
+def ceiled_sqrt(n):
+    if n==0 or n==1:
+        return n
+    start, stop = 1, n//2
+    while start < stop: 
+        mid = (start + stop) // 2
+        if mid*mid == n:
+            return mid
+        elif mid*mid < n:
+            if start == mid:
+                break
+            start = mid
+        else:
+            if stop == mid:
+                break
+            stop = mid
+    if mid*mid < n:
+        mid += 1 #When not perfect square mid should be incremented as there is a decimal part
+    return mid
 
 def even_square_generator(start, stop):
-	start = floored_sqrt(start)
+	start = ceiled_sqrt(start)
 	i = start+1 if start%2 else start
 	sq_i = i*i
 	while sq_i < stop:
@@ -33,7 +35,8 @@ def even_square_generator(start, stop):
 		i += 2
 		sq_i = i*i
 
-start = int(input("Start: "))
-stop = int(input("Stop: "))
-even_square_generator(start, stop)
+if __name__ == "__main__":
+    start = int(input("Start: "))
+    stop = int(input("Stop: "))
+    even_square_generator(start, stop)
 
